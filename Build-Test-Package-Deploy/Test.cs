ecee1781e5a816af902027ab8f23f54bde673c3f
balazs hideghety <balazs.hideghety@gmail.com>
2020-06-01 18:40:04 +0200

    Initial commit

---
+++ Build-Test-Package-Deploy/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Build_Test_Package_Deploy
+{
+    public class Test
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Running...");
+            Console.ReadKey();
+        }
+    }
+}
