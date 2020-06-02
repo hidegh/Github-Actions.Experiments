d67aa99954dbdd84163f698141f3c1692512da1b
balazs hideghety <balazs.hideghety@gmail.com>
2020-06-02 17:14:16 +0200

    Added a dependent package...

---
+++ Build-Test-Package-Deploy-Dependent/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Build_Test_Package_Deploy_Dependent
+{
+    public class Test
+    {
+        public static void Run()
+        {
+            Build_Test_Package_Deploy.Test.Run();
+
+            Console.WriteLine("Running...");
+            Console.ReadKey();
+        }
+    }
+}
