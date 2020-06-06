32cb353833f0bd87522ab5958af3fb3faf44e6e2
balazs hideghety <balazs.hideghety@gmail.com>
2020-06-06 21:16:01 +0200

    Cleanup

--- calculate-post-process.ps1
+++
@@ -1,8 +0,0 @@
-$out = .\calculate.ps1
-
-Write-Host "im here"
-Write-Host $out
-
-write ("encoding : {0}, mandatory: {1}" -f $out.encoding, $out.mandatory)
-
-write ("env. variable: {0}" -f $env:MY_VALUE)
