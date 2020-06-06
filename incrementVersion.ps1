32e35eee5a97e5ea30ebe0c2a4e688c9b3acef13
balazs hideghety <balazs.hideghety@gmail.com>
2020-06-06 21:13:10 +0200

    Added versioning build process

--- incrementVersion.ps1
+++ incrementVersion.ps1
@@ -1,11 +1,19 @@
+Write-Host "---"
+Write-Host "Incrementing version"
+
 $file = "Directory.Build.props"
 $xml = [xml](Get-Content $file)
 
 $version = [version] $xml.Project.PropertyGroup.Version
+Write-Host "From: $version"
 
 $newVersion = "{0}.{1}.{2}.{3}" -f $version.Major, $version.Minor, ($version.Build + 1), $version.Revision
-Write-Host $newVersion
+Write-Host "To  : $newVersion"
 
 $xml.Project.PropertyGroup.Version = $newVersion
 
 $xml.Save($file)
+Write-Host "Changes saved"
+
+echo "::set-env name=BUILDING_VERSION::$newVersion"
+Write-Host "Environment variable: BUILDING_VERSION was set"
