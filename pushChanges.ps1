598e90e9d84787982e85bcfa8d28c3de774bb454
[Build-process] <[build@process.id]>
2020-06-07 17:26:47 +0200

    Releasing

--- pushChanges.ps1
+++ pushChanges.ps1
@@ -10,12 +10,12 @@ $env:GIT_REDIRECT_STDERR = '2>&1'
 
 Write-Host "---"
 Write-Host "Get git status"
-$result = git status 2>&1
+$result = git status
 
 Write-Host "---"
 Write-Host "Set git identity"
-$result = git config user.name "[Build-process]" 2>&1
-$result = git config user.email "[build@process.id]" 2>&1
+$result = git config user.name "[Build-process]"
+$result = git config user.email "[build@process.id]"
 
 Write-Host "---"
 Write-Host "Commit"
