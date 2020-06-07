66fce55d20bef8d2fc71d7cdb88658e4434b7db9
[Build-process] <[build@process.id]>
2020-06-07 19:05:33 +0200

    Releasing

--- pushChanges.ps1
+++ pushChanges.ps1
@@ -11,28 +11,30 @@ param(
 Write-Host "`n---"
 Write-Host "Get git status"
 git status 2>&1 | Write-Host
+if (-not $?) { Write-Host "ERROR!"; exit -1; } # if ($LASTEXITCODE -ne 0) { exit -1; }
 
 Write-Host "`n---"
 Write-Host "Set git identity"
 git config user.name "[Build-process]" 2>&1 | Write-Host
 git config user.email "[build@process.id]" 2>&1 | Write-Host
+if (-not $?) { Write-Host "ERROR!"; exit -1; } 
 
 Write-Host "`n---"
 Write-Host "Commit"
 git commit -a -m "Releasing $versionParameter" 2>&1 | Write-Host
-if (-not $?) { exit -1; } # if ($LASTEXITCODE -ne 0) { exit -1; }
+if (-not $?) { Write-Host "ERROR!"; exit -1; } 
 
 Write-Host "`n---"
 Write-Host "Push"
 git push origin 2>&1 | Write-Host
-if (-not $?) { exit -1; } 
+if (-not $?) { Write-Host "ERROR!"; exit -1; } 
 
 Write-Host "`n---"
 Write-Host "Add tag"
 git tag -a $tag -m "Tag for new release" 2>&1 | Write-Host
-if (-not $?) { exit -1; } 
+if (-not $?) { Write-Host "ERROR!"; exit -1; } 
 
 Write-Host "`n---"
 Write-Host "Push tag"
 git push origin $tag 2>&1 | Write-Host
-if (-not $?) { exit -1; } 
+if (-not $?) { Write-Host "ERROR!"; exit -1; } 
