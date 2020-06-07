c08faffec87f41983de842471386ffeeff160938
[Build-process] <[build@process.id]>
2020-06-07 18:59:46 +0200

    redirect all

--- pushChanges.ps1
+++ pushChanges.ps1
@@ -6,34 +6,31 @@ param(
   [string]$tag
 )
 
-$env:GIT_REDIRECT_STDERR = '2>&1'
-
 Write-Host "`n---"
 Write-Host "Get git status"
-git status
+Write-Host git status 2>&1
 
 Write-Host "`n---"
 Write-Host "Set git identity"
-git config user.name "[Build-process]"
-git config user.email "[build@process.id]"
+Write-Host git config user.name "[Build-process]" 2>&1
+Write-Host git config user.email "[build@process.id]" 2>&1
 
 Write-Host "`n---"
 Write-Host "Commit"
-git commit -a -m "Releasing $versionParameter"
+Write-Host git commit -a -m "Releasing $versionParameter" 2>&1
 if (-not $?) { exit -1; } # if ($LASTEXITCODE -ne 0) { exit -1; }
 
 Write-Host "`n---"
 Write-Host "Push"
-$result = git push origin 2>&1
-Write-Host $result
+Write-Host git push origin 2>&1
 if (-not $?) { exit -1; } 
 
 Write-Host "`n---"
 Write-Host "Add tag"
-git tag -a $tag -m "Tag for new release"
+Write-Host git tag -a $tag -m "Tag for new release" 2>&1
 if (-not $?) { exit -1; } 
 
 Write-Host "`n---"
 Write-Host "Push tag"
-git push origin $tag
+Write-Host git push origin $tag 2>&1
 if (-not $?) { exit -1; } 
