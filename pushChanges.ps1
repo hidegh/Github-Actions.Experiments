0f315c0aa4e5f7bcbe9d4516fa74d8aa957bbeac
[Build-process] <[build@process.id]>
2020-06-07 19:02:03 +0200

    Fix

--- pushChanges.ps1
+++ pushChanges.ps1
@@ -8,29 +8,29 @@ param(
 
 Write-Host "`n---"
 Write-Host "Get git status"
-Write-Host git status 2>&1
+git status 2>&1 | Write-Host
 
 Write-Host "`n---"
 Write-Host "Set git identity"
-Write-Host git config user.name "[Build-process]" 2>&1
-Write-Host git config user.email "[build@process.id]" 2>&1
+git config user.name "[Build-process]" 2>&1 | Write-Host
+git config user.email "[build@process.id]" 2>&1 | Write-Host
 
 Write-Host "`n---"
 Write-Host "Commit"
-Write-Host git commit -a -m "Releasing $versionParameter" 2>&1
+git commit -a -m "Releasing $versionParameter" 2>&1 | Write-Host
 if (-not $?) { exit -1; } # if ($LASTEXITCODE -ne 0) { exit -1; }
 
 Write-Host "`n---"
 Write-Host "Push"
-Write-Host git push origin 2>&1
+git push origin 2>&1 | Write-Host
 if (-not $?) { exit -1; } 
 
 Write-Host "`n---"
 Write-Host "Add tag"
-Write-Host git tag -a $tag -m "Tag for new release" 2>&1
+git tag -a $tag -m "Tag for new release" 2>&1 | Write-Host
 if (-not $?) { exit -1; } 
 
 Write-Host "`n---"
 Write-Host "Push tag"
-Write-Host git push origin $tag 2>&1
+git push origin $tag 2>&1 | Write-Host
 if (-not $?) { exit -1; } 
