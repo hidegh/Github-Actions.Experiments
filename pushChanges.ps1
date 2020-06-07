74a86a9602de3287be67abe86c20671903799a61
[Build-process] <[build@process.id]>
2020-06-07 19:11:49 +0200

    Releasing

--- pushChanges.ps1
+++ pushChanges.ps1
@@ -42,8 +42,8 @@ Write-Host "Push tag"
 git push origin master $tag 2>&1 | Write-Host
 # The generic check-error code does not worked with push, raised error even on success!
 # if (-not $?) { Write-Host "ERROR!"; exit -1; } 
-if ($LASTEXITCODE -ne 0) { Write-Host "ERROR!"; }
-if (-not $?) { Write-Host "ERROR!"; }
+if ($LASTEXITCODE -ne 0) { Write-Host "ERROR1!"; }
+if (-not $?) { Write-Host "ERROR2!"; }
 
 Write-Host $LASTEXITCODE
 Write-Host $?
