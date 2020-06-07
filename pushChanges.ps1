d3e127cfc25ae5d2fd793188e9aec146bec9c1b4
balazs.hideghety <balazs.hideghety>
2020-06-07 10:19:44 +0200

    Try

--- pushChanges.ps1
+++ pushChanges.ps1
@@ -1,23 +1,32 @@
 param(
   [Parameter(Mandatory=$true)]
   $versionParameter
+  ,
+  [Parameter(Mandatory=$true)]
+  $tag
 )
 
+Write-Host "---"
 Write-Host "Get git status"
 git status
 
+Write-Host "---"
 Write-Host "Set git identity"
 git config user.name "[Build-process]"
 git config user.email "[build@process.id]"
 
+Write-Host "---"
 Write-Host "Commit"
-git commit -a -m "Releasing $env:BUILDING_VERSION"
+git commit -a -m "Releasing $versionParameter"
 
+Write-Host "---"
 Write-Host "Push"
 git push origin master
 
+Write-Host "---"
 Write-Host "Add tag"
-git tag -a "PROD-$env:BUILDING_VERSION" -m "Tag for new release"
+git tag -a $tag -m "Tag for new release"
 
+Write-Host "---"
 Write-Host "Push tag"
-git push origin "PROD-$env:BUILDING_VERSION"
+git push origin $tag
