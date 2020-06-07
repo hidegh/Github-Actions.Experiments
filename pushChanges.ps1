49511d543a1cd825b771cc447925cb47c252bd69
[Build-process] <[build@process.id]>
2020-06-07 12:08:27 +0200

    Further exp.

--- pushChanges.ps1
+++ pushChanges.ps1
@@ -20,7 +20,7 @@ $result = git config user.email "[build@process.id]" 2>&1
 Write-Host "---"
 Write-Host "Commit"
 $result = git commit -a -m "Releasing $versionParameter" 2>&1
-if (-not $?) { Write-Error $result }
+if (-not $?) { Write-Error $result; throw; }
 #if (-not $?) { throw } # if ($LASTEXITCODE -ne 0) { Write-Error $LASTEXITCODE; throw; }
 
 Write-Host "---"
@@ -29,7 +29,7 @@ $result = git push origin master 2>&1
 Write-Host "a"
 Write-Host $_
 Write-Host "b"
-if (-not $?) { Write-Error $result }
+if (-not $?) { Write-Error $result; throw; }
 #if ($LASTEXITCODE -ne 0) { Write-Error "cccc"; throw; }
 
 #if (-not $?) { throw }
@@ -37,11 +37,11 @@ if (-not $?) { Write-Error $result }
 Write-Host "---"
 Write-Host "Add tag"
 $result = git tag -a $tag -m "Tag for new release" 2>&1
-if (-not $?) { Write-Error $result }
+if (-not $?) { Write-Error $result; throw; }
 #if (-not $?) { throw }
 
 Write-Host "---"
 Write-Host "Push tag"
 $result = git push origin $tag 2>&1
-if (-not $?) { Write-Error $result }
+if (-not $?) { Write-Error $result; throw; }
 #if (-not $?) { throw }
