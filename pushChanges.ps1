92dfda68405d6cfdbcbd29897523fea056e59cfc
[Build-process] <[build@process.id]>
2020-06-07 12:06:26 +0200

    Releasing

--- pushChanges.ps1
+++ pushChanges.ps1
@@ -10,34 +10,38 @@ $env:GIT_REDIRECT_STDERR = '2>&1'
 
 Write-Host "---"
 Write-Host "Get git status"
-git status
+$result = git status 2>&1
 
 Write-Host "---"
 Write-Host "Set git identity"
-git config user.name "[Build-process]"
-git config user.email "[build@process.id]"
+$result = git config user.name "[Build-process]" 2>&1
+$result = git config user.email "[build@process.id]" 2>&1
 
 Write-Host "---"
 Write-Host "Commit"
-git commit -a -m "Releasing $versionParameter"
+$result = git commit -a -m "Releasing $versionParameter" 2>&1
+if (-not $?) { Write-Error $result }
 #if (-not $?) { throw } # if ($LASTEXITCODE -ne 0) { Write-Error $LASTEXITCODE; throw; }
 
 Write-Host "---"
 Write-Host "Push"
-git push origin master
+$result = git push origin master 2>&1
 Write-Host "a"
 Write-Host $_
 Write-Host "b"
+if (-not $?) { Write-Error $result }
 #if ($LASTEXITCODE -ne 0) { Write-Error "cccc"; throw; }
 
 #if (-not $?) { throw }
 
 Write-Host "---"
 Write-Host "Add tag"
-git tag -a $tag -m "Tag for new release"
+$result = git tag -a $tag -m "Tag for new release" 2>&1
+if (-not $?) { Write-Error $result }
 #if (-not $?) { throw }
 
 Write-Host "---"
 Write-Host "Push tag"
-git push origin $tag
+$result = git push origin $tag 2>&1
+if (-not $?) { Write-Error $result }
 #if (-not $?) { throw }
