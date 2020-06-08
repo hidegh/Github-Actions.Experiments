66ae6f035921e348e37c2b2a2e3799418bbce9fc
[Build-process] <[build@process.id]>
2020-06-08 11:43:50 +0200

    Fix nupkg publish script

--- publish.ps1
+++ publish.ps1
@@ -19,9 +19,9 @@ Write-Host "Publishing NuGet from projects..."
   $packageSourceFolder = "$($project)/$($nugetFolder)"
 
   Write-Host "`n`n`n"
-  Write-Host "Project       : $(${source})"
+  Write-Host "Project       : $(${project})"
   Write-Host "Package folder: $(${packageSourceFolder})"
 
-  dotnet nuget push -k $nuget_api_key -s $nugetSource $packageSourceFolder/*.nupkg --skip-duplicate 2>&1 | Write-Host
+  dotnet nuget push -Source $nugetSource -ApiKey $nuget_api_key $packageSourceFolder/*.nupkg --skip-duplicate 2>&1 | Write-Host
   if ($LASTEXITCODE -ne 0) { Write-Host "ERROR!"; exit -1; }
 }
